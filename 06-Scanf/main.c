f3b6e4310168e8b9abe9ab3a06013ac05ed3e3e9
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-06 20:57:38 -0300

    Scanf parte 1

---
+++ 06-Scanf/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char var_char;
+
+    int var_int;
+
+    float var_float;
+
+    scanf("%c %d %f", &var_char, &var_int, &var_float);
+
+    return 0;
+}
