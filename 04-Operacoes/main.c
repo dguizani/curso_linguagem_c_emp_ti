e8968fc89006ef16376df2405eeb3e972aaece61
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-06 20:06:21 -0300

    Operacoes parte 1

---
+++ 04-Operacoes/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int idade;
+    int matricula;
+
+    float preco;
+
+    printf("idade = %d\n", idade);
+    printf("matricula = %d\n", matricula);
+    printf("preco = %.2f\n", preco);
+
+    return 0;
+}
