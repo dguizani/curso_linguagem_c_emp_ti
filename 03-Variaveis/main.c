16b7f988e45dcf46a4c22ca4a304767ea7d9db5a
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-06 20:01:34 -0300

    Variaveis - Parte 2

--- 03-Variaveis/main.c
+++ 03-Variaveis/main.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-    int taxa = 18;
+    int idade = 14;
+    float peso = 70.8;
+    char sexo = 'F';
+    double taxa = 15.359742;
 
-    printf("\nGrupo 1:");
-    printf("\nJoao %d porcento", taxa);
-    printf("\nMaria %d porcento", taxa);
-    printf("\nJoaguim %d porcento", taxa);
-    printf("\nAbel %d porcento", taxa);
+    printf("\n A pessoa tal com idade %d que pesa %.2f kg eh do sexo %c e tera que pagar uma taxa de %lf %%\n",
+           idade, peso, sexo, taxa);
 
     return 0;
 }
