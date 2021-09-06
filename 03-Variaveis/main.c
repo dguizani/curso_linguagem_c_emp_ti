0134e637fd712fdca54974d1807ee23bff0ccc63
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-06 19:59:04 -0300

    Variaveis - Parte 1

---
+++ 03-Variaveis/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int taxa = 18;
+
+    printf("\nGrupo 1:");
+    printf("\nJoao %d porcento", taxa);
+    printf("\nMaria %d porcento", taxa);
+    printf("\nJoaguim %d porcento", taxa);
+    printf("\nAbel %d porcento", taxa);
+
+    return 0;
+}
