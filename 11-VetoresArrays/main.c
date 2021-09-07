f3432c636c2e4ce81d0777a580a3bf4f431ac816
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-07 01:00:59 -0300

    VetoresArrays parte 7

--- 11-VetoresArrays/main.c
+++ 11-VetoresArrays/main.c
@@ -3,23 +3,58 @@
 int main(int argc, char const *argv[])
 {
     /*
-        0 -> primeira posição
-        1 -> segunda posição
-        2 -> terceira posição
-        3 -> quarta posição
+        0 -> Nota 1
+        1 -> Nota 2
+        2 -> Nota 3
+        3 -> Nota 4
     */
-
-   int notas_aluno[4] = {
-        6, // pos 0
-        8, // pos 1
-        7  // pos 2
-    };
-
-    int frase[] = "Media das notas %d";
+    float notas[4];
     
-    notas_aluno[3] = (notas_aluno[0] + notas_aluno[1] + notas_aluno[2]) / 3;
+    /*
+        0 -> Peso Nota 1
+        1 -> Peso Nota 2
+        2 -> Peso Nota 3
+        3 -> Soma dos Pesos
+    */
+    int pesos[4];
+    
+    /*
+        Nome do Aluno
+    */
+    char nome[25];
 
-    printf(frase, notas_aluno[3]);
+    printf("\n Digite o nome do aluno: ");
+    scanf(" %[^\n]s", nome);
+
+    printf("\n Digite as 3 notas do aluno.");
+    printf("\n Nota 1: ");
+    scanf("%f", &notas[0]);
+
+    printf("\n Nota 2: ");
+    scanf("%f", &notas[1]);
+
+    printf("\n Nota 3: ");
+    scanf("%f", &notas[2]);
+
+    printf("\n Digite os pesos de cada nota do aluno.");
+    printf("\n Peso 1: ");
+    scanf("%d", &pesos[0]);
+
+    printf("\n Peso 2: ");
+    scanf("%d", &pesos[1]);
+
+    printf("\n Peso 3: ");
+    scanf("%d", &pesos[2]);
+
+    pesos[3] = pesos[0] + pesos[1] + pesos[2];
+
+    notas[3] = (notas[0] * pesos[0] + notas[1] * pesos[1] + notas[2] * pesos[2]) / pesos[3];
+
+    printf("\n Dados do aluno %s.", nome);
+    printf("\n Nota prova 1: %.2f", notas[0]);
+    printf("\n Nota prova 2: %.2f", notas[1]);
+    printf("\n Nota prova 3: %.2f", notas[2]);
+    printf("\n Media: %.2f", notas[3]);
 
     return 0;
 }
