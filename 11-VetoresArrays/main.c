2dbfabe88c3392dcb09e1bf6e03adacb09ed0b10
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-07 00:40:13 -0300

    VetoresArrays parte 3

--- 11-VetoresArrays/main.c
+++ 11-VetoresArrays/main.c
@@ -10,15 +10,18 @@ int main(int argc, char const *argv[])
         0 -> primeira posição
         1 -> segunda posição
         2 -> terceira posição
+        3 -> quarta posição
     */
 
-    float notas_aluno[] = {
+    float notas_aluno[4] = {
         6.6, // pos 0
         8,   // pos 1
         7    // pos 2
     };
 
-    printf("\nMedia das notas: %.2f", (notas_aluno[0] + notas_aluno[1] + notas_aluno[2]) / 3);
+    notas_aluno[3] = (notas_aluno[0] + notas_aluno[1] + notas_aluno[2]) / 3;
+
+    printf("\nMedia das notas: %.2f", notas_aluno[3]);
 
     return 0;
 }
