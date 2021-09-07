593bb37f2989d979b9975b8cd437bdc658edb8e7
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-06 21:06:40 -0300

    Scanf parte 4

--- 06-Scanf/main.c
+++ 06-Scanf/main.c
@@ -2,16 +2,12 @@
 
 int main(int argc, char const *argv[])
 {
-    char var_char1;
-    char var_char2;
+    char var_char1 = '\n';
+    char var_char2 = 10;
 
     int var_int;
 
     float var_float;
-
-    scanf(" %c", &var_char1);
-
-    scanf(" %c", &var_char2);
 
     printf("'%c' '%c'", var_char1, var_char2);
 
