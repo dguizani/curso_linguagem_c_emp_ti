9834ffe88c7e72ff1248bb4703d57b2d7f873340
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-07 17:25:53 -0300

    DoWhileEWhile parte 1

---
+++ 13-DoWhileEWhile/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int contador = 0;
+
+    while (contador < 5)
+    {
+        printf("\nUma frase legal");
+        contador += 1;
+    }
+
+    return 0;
+}
