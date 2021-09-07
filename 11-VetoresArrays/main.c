c929ca1a739acfbfdd0e771edb997c9c6d93c31a
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-07 00:47:13 -0300

    VetoresArrays parte 5

--- 11-VetoresArrays/main.c
+++ 11-VetoresArrays/main.c
@@ -9,12 +9,35 @@ int main(int argc, char const *argv[])
         3 -> quarta posição
     */
 
-    int notas_aluno[4] = {
+   int notas_aluno[4] = {
         6, // pos 0
         8, // pos 1
         7  // pos 2
     };
 
+    int frase[] = {
+        'M',
+        'e',
+        'd',
+        'i',
+        'a',
+        ' ',
+        'd',
+        'a',
+        's',
+        ' ',
+        'n',
+        'o',
+        't',
+        'a',
+        's',
+        ':',
+        ' ',
+        '%',
+        'd',
+        '\0'
+    };
+    
     notas_aluno[3] = (notas_aluno[0] + notas_aluno[1] + notas_aluno[2]) / 3;
 
     printf("\nMedia das notas: %d", notas_aluno[3]);
