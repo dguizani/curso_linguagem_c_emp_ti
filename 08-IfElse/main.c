a5e1a9700111db7f9ede9053de343d1b2991a0ed
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-06 21:56:15 -0300

    IfElse parte 3
    Operador lógico ou (||)

--- 08-IfElse/main.c
+++ 08-IfElse/main.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char const *argv[])
 {
-    //     falso ou false -> 0
-    // verdadeiro ou true -> 1
+    //      falso ou false -> 0
+    // verdadeiro ou true  -> 1
 
     // E ou AND -> &&
 
@@ -12,11 +12,17 @@ int main(int argc, char const *argv[])
     // 0 && 0 -> 0
     // 1 && 1 -> 1
 
-    printf("\n%d", 1 && 0);
-    printf("\n%d", 0 && 1);
-    printf("\n%d", 0 && 0);
-    printf("\n%d", 1 && 1);
+    // OU ou OR -> ||
+
+    // 1 || 0 -> 1
+    // 0 || 1 -> 1
+    // 0 || 0 -> 0
+    // 1 || 1 -> 1
+
+    printf("\n%d", 1 || 0);
+    printf("\n%d", 0 || 1);
+    printf("\n%d", 0 || 0);
+    printf("\n%d", 1 || 1);
     
     return 0;
 }
-
