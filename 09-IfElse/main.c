9811a88f5393f11499ed1db1bbca4498e14bef05
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-07 00:19:20 -0300

    IfElse parte 13

--- 09-IfElse/main.c
+++ 09-IfElse/main.c
@@ -5,24 +5,41 @@ int main(int argc, char const *argv[])
     // 1 -> verdade
     // 0 -> falso
 
-    // 15 10 -> 10 15
-    // 5 10  -> 5 10
-    // 10 10 -> 10 10
+    char opcao;
 
-    int num1;
-    int num2;
+    printf("\n+--------------------------+");
+    printf("\n|%*s%s%*s|", 11, "", "MENU", 11, "");
+    printf("\n+--------------------------+");
+    printf("\n| %d | %-*s |", 1, 20, "Opcao 1");
+    printf("\n| %d | %-*s |", 2, 20, "Opcao 2");
+    printf("\n| %d | %-*s |", 3, 20, "Opcao 3");
+    printf("\n| %d | %-*s |", 4, 20, "Opcao 4");
+    printf("\n| %d | %-*s |", 5, 20, "Opcao 5");
+    printf("\n| %d | %-*s |", 6, 20, "Opcao 6");
+    printf("\n+--------------------------+");
 
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &num1);
+    scanf("%c", &opcao);
+
+    if (opcao == '1')
+        printf("\nOpcao 1");
     
-    printf("\nDigite o segundo numero: ");
-    scanf("%d", &num2);
-
-    if (num1 < num2)
-        printf("\n%d %d", num1, num2);
+    else if (opcao == '2')
+        printf("\nOpcao 2");
+    
+    else if (opcao == '3')
+        printf("\nOpcao 3");
+    
+    else if (opcao == '4')
+        printf("\nOpcao 4");
+    
+    else if (opcao == '5')
+        printf("\nOpcao 5");
+    
+    else if (opcao == '6')
+        printf("\nOpcao 6");
     
     else
-        printf("\n%d %d", num2, num1);
+        printf("\nOpcao Invalida");
 
     return 0;
 }
