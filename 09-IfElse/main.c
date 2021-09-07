34db60ae8a8e9cab9c1da5297fadac043460879c
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-07 00:09:31 -0300

    IfElse parte 12

--- 09-IfElse/main.c
+++ 09-IfElse/main.c
@@ -5,19 +5,24 @@ int main(int argc, char const *argv[])
     // 1 -> verdade
     // 0 -> falso
 
-    if (0 < 1)
-    {
-        printf("\ncomando if 1");
-        printf("\ncomando if 2");
-    }
+    // 15 10 -> 10 15
+    // 5 10  -> 5 10
+    // 10 10 -> 10 10
 
+    int num1;
+    int num2;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+    
+    printf("\nDigite o segundo numero: ");
+    scanf("%d", &num2);
+
+    if (num1 < num2)
+        printf("\n%d %d", num1, num2);
+    
     else
-    {
-        printf("\ncomando else 1");
-        printf("\ncomando else 2");
-    }
-
-    printf("\nfora do if else");
+        printf("\n%d %d", num2, num1);
 
     return 0;
 }
