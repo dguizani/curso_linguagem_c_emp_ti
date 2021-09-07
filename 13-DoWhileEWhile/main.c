1ddc240b5d34272c64157871a99509d61ecb9cd1
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-07 17:37:58 -0300

    DoWhileEWhile parte 2

--- 13-DoWhileEWhile/main.c
+++ 13-DoWhileEWhile/main.c
@@ -2,13 +2,19 @@
 
 int main(int argc, char const *argv[])
 {
-    int contador = 0;
+    int contador = 5;
 
     while (contador < 5)
     {
-        printf("\nUma frase legal");
+        printf("\nWhile");
         contador += 1;
     }
+
+    do
+    {
+        printf("\nDo While");
+        contador += 1;
+    } while (contador < 5);
 
     return 0;
 }
