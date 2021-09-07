5440d4db175bb8c7de440d041b442e90fd74be35
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-09-06 22:01:08 -0300

    IfElse parte 5
    Operador lógico menor que (<)

--- 08-IfElse/main.c
+++ 08-IfElse/main.c
@@ -24,8 +24,15 @@ int main(int argc, char const *argv[])
     // 15 > 10 -> 1
     // 15 > 87 -> 0
 
-    printf("\n%d", 15 > 10);
-    printf("\n%d", 15 > 87);
+    // menor que -> "<"
+
+    // 15 < 10 -> 0
+    // 15 < 87 -> 1
+    // 10 < 10 -> 0
+
+    printf("\n%d", 15 < 10);
+    printf("\n%d", 15 < 87);
+    printf("\n%d", 10 < 10);
     
     return 0;
 }
