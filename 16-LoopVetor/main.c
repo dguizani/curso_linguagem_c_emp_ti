8c7cd97e1fadff224ea47085e9d8803908edace8
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-10-09 15:12:17 -0300

    LoopVetorMatriz parte 1

---
+++ 16-LoopVetor/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float notas[5];
+
+    printf("Digite a nota 1: ");
+    scanf("%f", &notas[0]);
+
+    printf("Digite a nota 2: ");
+    scanf("%f", &notas[1]);
+
+    printf("Digite a nota 3: ");
+    scanf("%f", &notas[2]);
+
+    printf("Digite a nota 4: ");
+    scanf("%f", &notas[3]);
+
+    printf("Digite a nota 5: ");
+    scanf("%f", &notas[4]);
+
+    return 0;
+}
