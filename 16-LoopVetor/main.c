e2259aa8fe7df5b19c3cd4c10213d295cfafbab7
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-10-09 16:25:24 -0300

    LoopVetorMatriz parte 4

--- 16-LoopVetor/main.c
+++ 16-LoopVetor/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
+#define TAM_VETOR 8
+
 int main(int argc, char const *argv[])
 {
-    float notas[5];
+    float notas[TAM_VETOR];
 
     float media = 0;
 
-    int pos_nota = 1;
+    int pos_nota = 0;
 
     /*
         numero da nota   -> 1 2 3 4 5 ....
         posição do vetor -> 0 1 2 3 4 ....
     */
 
-    while (pos_nota != 6)
+    while (pos_nota != TAM_VETOR)
     {
         printf("Digite a nota %d: ", pos_nota);
         scanf("%f", &notas[pos_nota - 1]);
@@ -23,12 +25,12 @@ int main(int argc, char const *argv[])
 
     // 5 7 4 2 1
 
-    for (pos_nota = 0; pos_nota != 5; pos_nota += 1)
+    for (pos_nota = 0; pos_nota != TAM_VETOR; pos_nota += 1)
         media += notas[pos_nota];
 
-    media /= 5;
+    media /= TAM_VETOR;
 
-    for (pos_nota = 0; pos_nota != 5; pos_nota += 1)
+    for (pos_nota = 0; pos_nota != TAM_VETOR; pos_nota += 1)
         printf("%f ", notas[pos_nota]);
     
     printf("\na media das notas eh %.2f", media);
