92ce4c734a65e1b8ec9131ab470ec2165716a2c2
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-10-24 14:35:35 -0300

    LoopVetorMatriz parte 5

---
+++ 17-LoopMatriz/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define  NUM_LINHAS 3
+#define NUM_COLUNAS 3
+
+int main(int argc, char const *argv[])
+{
+    char matriz[NUM_LINHAS][NUM_COLUNAS];
+
+    int linha = 0;
+    int coluna = 0;
+
+    /*
+         linhas: 0 0 0 1 1 1 2 2 2
+        colunas: 0 1 2 0 1 2 0 1 2
+    */
+
+    /*
+         linha: 3
+        coluna: 0
+    */
+
+    while (linha != NUM_LINHAS)
+    {
+        while (coluna != NUM_COLUNAS)
+        {
+            matriz[linha][coluna] = '_';
+
+            coluna += 1;
+        }
+
+        coluna = 0;
+        linha += 1;
+    }
+
+    linha = 0;
+
+    while (linha != NUM_LINHAS)
+    {
+        while (coluna != NUM_COLUNAS)
+        {
+            printf("%c ", matriz[linha][coluna]);
+
+            coluna += 1;
+        }
+
+        coluna = 0;
+        linha += 1;
+    }
+
+    return 0;
+}
